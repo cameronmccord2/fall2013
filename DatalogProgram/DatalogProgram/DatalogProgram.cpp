86289138cda5a6f4973814a24e0a230965a68e9e
cmccord <cameronmccord2@gmail.com>
2013-09-25 09:24:01 -0600

    chagned command line stuff

--- DatalogProgram/DatalogProgram/DatalogProgram.cpp
+++ DatalogProgram/DatalogProgram/DatalogProgram.cpp
@@ -130,11 +130,11 @@ int main(int argc, char* argv[]) {
 	Lex *lex = NULL;
     DatalogProgram *dp = NULL;
 	try{
-//        lex = new Lex(argv[1]);
-        lex = new Lex("/Users/taylormccord/Dropbox/Eclipse Workspace/fall2013/DatalogProgram/DatalogProgram/in21.txt");
+        lex = new Lex(argv[1]);
+        //lex = new Lex("/Users/taylormccord/Dropbox/Eclipse Workspace/fall2013/DatalogProgram/DatalogProgram/in21.txt");
         dp = new DatalogProgram(lex->getTokens());
-//        dp->writeToFile(argv[2]);
-        cout << dp->toString();
+        dp->writeToFile(argv[2]);
+        //cout << dp->toString();
 	}catch(exception& e){
 		e.what();
 	}
